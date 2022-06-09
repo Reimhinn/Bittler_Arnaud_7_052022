16d23a4c92acba495315fa7a19ed5ac710cc1827
Reimhinn <arnaud.bittler@gmail.com>
2022-06-09 11:35:17 +0200

    display ingredient and device

--- index.mjs
+++ index.mjs
@@ -1,10 +1,12 @@
 import recipes from './recipes.mjs'
 import { cardFactory } from './card.mjs'
 import { listFactory } from './list.mjs'
+import { displayIngredientList } from './list.mjs'
+import { displayDeviceList } from './list.mjs'
 
 const cards = document.querySelector('.cards')
 
-function showCards() {
+function showCards () {
   recipes.forEach(recipe => {
     let cardModel = cardFactory(recipe)
     let cardModelDOM = cardModel.getCardDOM()
@@ -18,33 +20,30 @@ const dropdowns = document.querySelectorAll('.dropdown')
 
 dropdowns.forEach(dropdown => {
   dropdown.addEventListener('click', event => {
-   if (event.target.classList.contains('first-dropdown')) {
-    displayIngredientList(recipes)
-   }
+    if (event.target.classList.contains('first-dropdown')) {
+      displayIngredientList(recipes)
+    }
+    else if (event.target.classList.contains('second-dropdown')){
+      displayDeviceList(recipes)
+    }
   })
+})
 
-});
-
-
-
-function displayIngredientList (data) {
-  const ingredientListContainer = document.querySelector('.ingredient-list-container')
-  data.forEach(recipe => {
-    let listModel = listFactory(recipe)
-    let ingredientListModelDOM = listModel.getIngredientListDOM()
-    ingredientListContainer.appendChild(ingredientListModelDOM)
+document.querySelectorAll('.dropdown-arrow').forEach(arrow => {
+  arrow.addEventListener('click', event => {
+    if (event.target.parentNode.classList.contains('first-dropdown')) {
+      console.log(event.target.parentNode)
+      displayIngredientList(recipes)
+    }
   })
+})
 
-  const firstButton = document.querySelector('.first-button')
-  const ingredientButtonContainer = document.querySelector('.ingredient-button-container')
+document.querySelectorAll('.button').forEach(btn => {
+  btn.addEventListener('click', event => {
+    if (event.target.classList.contains('first-button')) {
+      displayIngredientList(recipes)
+    }
+  })
+})
 
-  let ingredientDisplayValue = window.getComputedStyle(ingredientListContainer).display;
 
-  if (ingredientDisplayValue === 'none') {
-    ingredientListContainer.style.display = 'block'
-    firstButton.style.width = '40vw'
-    ingredientButtonContainer.style.width = '40vw'
-} else {ingredientListContainer.style.display = 'none'
-    firstButton.style.width = '110px'
-    ingredientButtonContainer.style.width = '150px'}
-}
--- list.mjs
+++ list.mjs
@@ -9,17 +9,87 @@ export function listFactory (data) {
     ustensils
   } = data
 
+  let deviceArray = []
+
   function getIngredientListDOM () {
     let ingredientList = document.querySelector('.ingredient-list')
     ingredients.forEach(ingredientObject => {
       let ingredientListElement = document.createElement('li')
       ingredientListElement.textContent += ingredientObject.ingredient
-      console.log(ingredientListElement)
       ingredientList.appendChild(ingredientListElement)
     })
 
     return ingredientList
   }
 
-  return { ...data, getIngredientListDOM }
+  function getDeviceListDOM () {
+    let deviceList = document.querySelector('.device-list')
+    let deviceListElement = document.createElement('li')
+
+    deviceArray.push(appliance)
+    deviceListElement.textContent += appliance
+
+    deviceList.appendChild(deviceListElement)
+
+    return deviceList
+  }
+
+  return { ...data, getIngredientListDOM, getDeviceListDOM, deviceArray }
+}
+
+let listModel = getDeviceListDOM()
+
+export function displayIngredientList (data) {
+  const ingredientListContainer = document.querySelector(
+    '.ingredient-list-container'
+  )
+  data.forEach(recipe => {
+    let listModel = listFactory(recipe)
+    let ingredientListModelDOM = listModel.getIngredientListDOM()
+    ingredientListContainer.appendChild(ingredientListModelDOM)
+  })
+
+  const firstButton = document.querySelector('.first-button')
+  const ingredientButtonContainer = document.querySelector(
+    '.ingredient-button-container'
+  )
+
+  let ingredientDisplayValue = window.getComputedStyle(ingredientListContainer)
+    .display
+
+  if (ingredientDisplayValue === 'none') {
+    ingredientListContainer.style.display = 'block'
+    firstButton.style.width = '40vw'
+    ingredientButtonContainer.style.width = '40vw'
+  } else {
+    ingredientListContainer.style.display = 'none'
+    firstButton.style.width = '110px'
+    ingredientButtonContainer.style.width = '150px'
+  }
+}
+
+export function displayDeviceList (data) {
+  const deviceListContainer = document.querySelector('.device-list-container')
+  data.forEach(recipe => {
+    let listModel = listFactory(recipe)
+    let deviceListModelDOM = listModel.getDeviceListDOM()
+    deviceListContainer.appendChild(deviceListModelDOM)
+  })
+
+  const secondButton = document.querySelector('.second-button')
+  const deviceButtonContainer = document.querySelector(
+    '.device-button-container'
+  )
+
+  let deviceDisplayValue = window.getComputedStyle(deviceListContainer).display
+
+  if (deviceDisplayValue === 'none') {
+    deviceListContainer.style.display = 'block'
+    secondButton.style.width = '40vw'
+    deviceButtonContainer.style.width = '40vw'
+  } else {
+    deviceListContainer.style.display = 'none'
+    secondButton.style.width = '110px'
+    deviceButtonContainer.style.width = '150px'
+  }
 }
